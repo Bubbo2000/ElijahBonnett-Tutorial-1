8814ee6e2fd86943c450e8ff6b864e5c2f70cba7
Bubbo2000 <89593644+Bubbo2000@users.noreply.github.com>
2021-09-15 15:08:34 -0400

    September 15, 2021 3:08 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        lives = 3;
         rb = GetComponent<Rigidbody>();
         count = 0;
 
