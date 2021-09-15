2844060ab957e117f89b74009816627b091fdb39
Bubbo2000 <89593644+Bubbo2000@users.noreply.github.com>
2021-09-15 15:06:32 -0400

    September 15, 2021 3:06 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
 
+    private int lives;
+
     // Start is called before the first frame update
     void Start()
     {
